bb8a0d988232bd55ca73302afc3c038812e60540
1heart <yixin1996@gmail.com>
2016-05-12 15:43:34 -0400

    Cleaned up stuff

--- lab3/lda_train.m
+++ lab3/lda_train.m
@@ -10,31 +10,19 @@ uniqueL = unique(labels);
 numL = length(uniqueL);
 
 X_i = cell(numL, 1);
+means = cell(numL, 1);
+p = cell(numL, 1);
+C_i = cell(numL, 1);
+C = zeros(d, 2);
+
 for i = 1:numL
   X_i{i} = X(labels == uniqueL(i), :);
-end
-
-means = cell(numL, 1);
-for i = 1:numL
   means{i} = mean(X_i{i});
-end
-
-p = cell(numL, 1);
-for i = 1:numL
   p{i} = sum(labels == uniqueL(i)) / n;
-end
-
-C_i = cell(numL, 1);
-for i = 1:numL
   C_i{i} = cov(X_i{i});
-end
-
-C = zeros(d);
-for i = 1:numL
   C = C + (p{i} * C_i{i});
 end
 
 invC = inv(C);
-
 b_hat = invC * (means{1}' - means{2}');
 c_hat = log(p{1} / p{2}) + 0.5 * (means{1} * invC * means{1}' - means{2} * invC * means{2}');
