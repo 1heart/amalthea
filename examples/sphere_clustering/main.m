4779b3cf70ca706eeebfc89da52dd691c93723f0
1heart <yixin1996@gmail.com>
2016-05-18 09:11:23 -0400

    Some work on spherical clustering

---
+++ examples/sphere_clustering/main.m
@@ -0,0 +1,10 @@
+addpath('circ_stat_2012a');
+
+[X, Y, Z] = sphere;
+surf(X,Y,Z);
+
+[d1x, d2x, d3x] = rand_circ_pts(1);
+
+numClusters = 2;
+
+
---
+++ examples/sphere_clustering/rand_angle.m
@@ -0,0 +1,3 @@
+function [theta] = rand_angle()
+theta = acos(1-2*rand(n,1));
+end
