595f374d9d7e330fb860fae5144b8ad57489bf22
1heart <yixin1996@gmail.com>
2016-05-23 16:11:36 -0400

    Print tree

--- code/hierarchical_clustering/print_tree.m
+++ code/hierarchical_clustering/print_tree.m
@@ -1,5 +1,19 @@
-function [] = print_tree(T)
+function [] = print_tree(T, level)
 
+if nargin < 2
+  level = 0;
+end
 
+for i = 1:size(T.mean,2)
+  fprintf(repmat(' ', 1, level * 4));
+  fprintf(num2str(T.mean(i)));
+  fprintf('\n');
+end
+fprintf('\n');
+
+children = T.children;
+for i = 1:size(children,2)
+  print_tree(children(i), level + 1);
+end
 
 end
