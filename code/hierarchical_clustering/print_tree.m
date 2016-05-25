66dc53ebf6245dfd78c559b96ef91b47fbfc870d
1heart <yixin1996@gmail.com>
2016-05-25 17:34:32 -0400

    Added printing ids in print_tree.m

--- code/hierarchical_clustering/print_tree.m
+++ code/hierarchical_clustering/print_tree.m
@@ -4,9 +4,18 @@ if nargin < 2
   level = 0;
 end
 
+prefix = repmat(' ', 1, level * 4);
+
 for i = 1:size(T.mean,2)
-  fprintf(repmat(' ', 1, level * 4));
+  fprintf(prefix);
   fprintf(num2str(T.mean(i)));
+  fprintf('\n');
+end
+fprintf(prefix);
+fprintf('---\n');
+for i = 1:size(T.ids,2)
+  fprintf(prefix);
+  fprintf(num2str(T.ids(i)));
   fprintf('\n');
 end
 fprintf('\n');
