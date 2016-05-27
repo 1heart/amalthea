451625fdecc6a03aadc71cac2cae655cfece2c22
1heart <yixin1996@gmail.com>
2016-05-27 16:44:26 -0400

    Added support for multiple data sets

--- code/hierarchical_clustering/metrics/run_metrics.m
+++ code/hierarchical_clustering/metrics/run_metrics.m
@@ -1,12 +1,22 @@
-D = [1 0 0; 0 1 0; 0 0 1;];
-L = [1; 1; 2;];
-
 tree_constructors = {@meantree, @meantree2, @link_tree};
 metrics = {@precision_recall};
 
+datasets = {};
+labels = {};
+
+datasets{1} = [1 0 0; 0 1 0; 0 0 1;];
+labels{1} = [1; 1; 2;];
+
 metric_results = {};
 
-for i = 1:length(tree_constructors)
-  metric_results{i} = eval_tree(D, L, metrics, tree_constructors{i});
+for i = 1:length(datasets)
+  D = datasets{i};
+  L = labels{i};
+  metric_result = {};
+  for j = 1:length(tree_constructors)
+    metric_result{j} = eval_tree(D, L, metrics, tree_constructors{j});
+  end
+  metric_results{i} = metric_result;
 end
+
 
