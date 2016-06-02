256936ebd03ebfd92a07150e722e248f94a09426
1heart <yixin1996@gmail.com>
2016-06-02 10:56:26 -0400

    Added selection of means for spherical data

--- code/hierarchical_clustering/utilities/random_spherical_data.m
+++ code/hierarchical_clustering/utilities/random_spherical_data.m
@@ -1,4 +1,16 @@
-function [dataMatrix, meanMatrix, memMatrix] = random_spherical_data(numClusters, numPoints, kappa)
+function [dataMatrix, meanMatrix, memMatrix] = random_spherical_data(numClusters, numPoints, kappa, means1, means2)
+
+if nargin <= 4
+  means1 = nan(numClusters); means2 = nan(numClusters);
+  for i = 1:numClusters
+    means1(i) = rand_angle();
+    means2(i) = rand_angle();
+  end
+end
+
+if length(means1) ~= numClusters || length(means2) ~= numClusters
+  error('Means vectors not the right length!');
+end
 
 % Kappa is "concentration parameter"
 
@@ -6,7 +18,7 @@ dataMatrix = [];
 meanMatrix = [];
 memMatrix = []; % membership matrix
 for i = 1:numClusters
-    mean1 = rand_angle(); mean2 = rand_angle();
+    mean1 = means1(i); mean2 = means2(i);
     angles = [circ_vmrnd(mean1, kappa, numPoints) circ_vmrnd(mean2, kappa, numPoints)];
     [x ,y, z] = sph2cart(angles(:,1), angles(:, 2), 1);
     [muX, muY, muZ] = sph2cart(mean1, mean2, 1);
