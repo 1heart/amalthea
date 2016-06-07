42ade9f7820a82bb6def4a23b122982e56535435
1heart <yixin1996@gmail.com>
2016-06-07 15:16:28 -0400

    WIP sphere_dist_linassign

--- code/linassgn/sphere_dist_linassgn.m
+++ code/linassgn/sphere_dist_linassgn.m
@@ -1,38 +1,15 @@
-function d = sphere_dist_linassgn(x, y, lambda)
+function d = sphere_dist_linassgn(x, y, distMatrix, lambda)
 
 if (~isvector(x) || ~isvector(y)) error('Not vector inputs!'); end;
 if (~isequal(length(x), length(y))) error('Not equal size vectors!'); end;
-
-% Make vectors column vectors
-if size(x,1) == 1 x = x'; end;
-if size(y,1) == 1 y = y'; end;
-
-% Default lambda value = 1
-if (nargin < 3) lambda = 1; end;
 n = length(x);
+if (~isequal(size(distMatrix), [n n])) error('Distance matrix not nxn!'); end;
+% Make vectors column vectors
+if (size(x,2) ~= 1) x = x'; end;
+if (size(y,2) ~= 1) y = y'; end;
 
-% Create distance matrix
-D = zeros(n);
-for i = 1:n
-  for j = 1:n
-    D(i,j) = abs(i-j);
-  end
-end
-
-% Create cost matrix
-
-C = zeros(n);
-for i = 1:n
-  for j = i:n
-    C(i,j) = abs(x(i) - y(j));
-    C(j,i) = C(i,j);
-  end
-end
-
-% C = x * y' + lambda * D;
-C = C + lambda * D;
-[rowsol, cost] = lapjv(C);
-
-d = sphere_norm(x(rowsol), y);
+C = -(x * y') + lambda * D; % Construct cost matrix
+[rowsol, cost] = lapjv(C); % Get best linear assignment from x to y
+d = sphere_norm(x(rowsol), y); % Take the distance between y and the modified x
 
 end
