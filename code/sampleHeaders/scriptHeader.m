5568eabc93d2db016f7510d35254626e7352b6d5
1heart <yixin1996@gmail.com>
2016-06-08 13:11:27 -0400

    Added some edits to sample headers

--- code/sampleHeaders/scriptHeader.m
+++ code/sampleHeaders/scriptHeader.m
@@ -9,5 +9,5 @@
 % Description: This driver computes the 3D square root wavelet density
 %              coefficients and densities of 3D data. These coefficients 
 %              are then used in a shape matching framework.
-% Usage: This program is used in the 3D shape matching research framework.
+% Usage: Used in hierarchical clustering on the unit hypersphere.
 % -------------------------------------------------------------------------
