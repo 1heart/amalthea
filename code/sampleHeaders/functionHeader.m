b6c97354d656364967a72520a211c226ccf9d1df
1heart <yixin1996@gmail.com>
2016-06-08 11:50:03 -0400

    Added Mark's function/script headers

---
+++ code/sampleHeaders/functionHeader.m
@@ -0,0 +1,26 @@
+%--------------------------------------------------------------------------
+% Function:    getDateString
+% Description: This function creates a string without spaces that contains
+%              the date and time.
+% 
+% Inputs: 
+%
+%   in1     - Example input 1. 
+%
+%   in2     - Example input 2. 
+% 
+% Outputs
+% 
+%   dateStr - String that contains the date and time without white spaces.
+%
+% Usage: Used in the affine invariance framework.
+%
+% Authors(s):
+%   Mark Moyou - markmmoyou@gmail.com
+%
+% Date: Monday 6th June, 2016 (2:34pm)
+%
+% Affiliation: Florida Institute of Technology. Information
+%              Characterization and Exploitation Laborartory.
+%              http://research2.fit.edu/ice/
+% -------------------------------------------------------------------------
---
+++ code/sampleHeaders/scriptHeader.m
@@ -0,0 +1,12 @@
+% -------------------------------------------------------------------------
+% Script: Testing3DWDE_Par (Par is parallel).
+% Date: Wednesday 16th March, 2016. 
+% Author: Mark Moyou (mmoyou@my.fit.edu)
+% Affiliation: Florida Institute of Technology. Information
+%              Characterization and Exploitation Laborartory.
+%              http://research2.fit.edu/ice/
+% Description: This driver computes the 3D square root wavelet density
+%              coefficients and densities of 3D data. These coefficients 
+%              are then used in a shape matching framework.
+% Usage: This program is used in the 3D shape matching research framework.
+% -------------------------------------------------------------------------
