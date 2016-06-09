2dd74090d6b08395286dccfa285827507f88c8e4
1heart <yixin1996@gmail.com>
2016-06-09 15:51:01 -0400

    Fix for sphere_dist_linassgn

--- code/linassgn/sphere_dist_linassgn.m
+++ code/linassgn/sphere_dist_linassgn.m
@@ -42,7 +42,7 @@ if (~isequal(size(distMatrix), [n n])) error('Distance matrix not nxn!'); end;
 if (size(x,2) ~= 1) x = x'; end;
 if (size(y,2) ~= 1) y = y'; end;
 
-C = -(x * y') + lambda * D; % Construct cost matrix
+C = -(x * y') + lambda * distMatrix; % Construct cost matrix
 [rowsol, cost] = lapjv(C); % Get best linear assignment from x to y
 d = sphere_norm(x(rowsol), y); % Take the distance between y and the modified x
 
