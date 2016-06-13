3f3e23659d7ba29c3920310acdfa37da07ccb822
1heart <yixin1996@gmail.com>
2016-06-13 16:52:11 -0400

    Added multires for sphere_dist_linassgn

--- code/linassgn/sphere_dist_linassgn.m
+++ code/linassgn/sphere_dist_linassgn.m
@@ -8,14 +8,18 @@
 %
 %	x,y 			- Vectors of points on the hypersphere
 % 
-%	distMatrix		- The Euclidean distance between the points that each
-% 						dimension of the hypersphere represents
+%	distMatrix		- A cell array of the Euclidean distance between the points that each
+% 						dimension of the hypersphere represents, at each resolution level.
 %
 % 	lambda 			- The weighting of the distance matrix on the final result
 %
+% 	multires_indices        - An mx2 array of [start_i, end_i] that represent each
+%                                  segment of the vector that corresponds to each resolution level.
+%                                  Defaults to [1 end].
+%
 % Outputs
 % 
-%	d 		 		- The spherical distance between
+%	 		 		- The spherical distance between
 % 						the two points after linear assignment
 %
 % Usage: Used in hierarchical retrieval on the unit hypersphere.
@@ -32,18 +36,26 @@
 %              http://research2.fit.edu/ice/
 % -------------------------------------------------------------------------
 
-function d = sphere_dist_linassgn(x, y, distMatrix, lambda)
+function d = sphere_dist_linassgn(x, y, distMatrix, lambda, multires_indices)
 
 if (~isvector(x) || ~isvector(y)) error('Not vector inputs!'); end;
 if (~isequal(length(x), length(y))) error('Not equal size vectors!'); end;
 n = length(x);
-if (~isequal(size(distMatrix), [n n])) error('Distance matrix not nxn!'); end;
 % Make vectors column vectors
 if (size(x,2) ~= 1) x = x'; end;
 if (size(y,2) ~= 1) y = y'; end;
+if (nargin < 5) multires_indices = [1 length(x)]; end;
 
-C = -(x * y') + lambda * distMatrix; % Construct cost matrix
-[rowsol, cost] = lapjv(C); % Get best linear assignment from x to y
-d = sphere_norm(x(rowsol), y); % Take the distance between y and the modified x
+x_new = [];
+for res_level = 1:size(multires_indices, 1)
+  curri = multires_indices(res_level, :);
+  curr_range = curri(1):curri(2);
+  x_curr = x(curr_range); y_curr = y(curr_range);
+  C = -(x_curr * y_curr') + lambda * distMatrix{res_level}; % Construct cost matrix
+  [rowsol, cost] = lapjv(C); % Get best linear assignment from x to y
+  x_new = [x_new; x(rowsol)];
+end
+
+d = sphere_norm(x_new, y); % Take the distance between y and the modified x
 
 end
