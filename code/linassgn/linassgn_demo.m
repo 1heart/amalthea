533ed0a8e123edde3c89e1a23857ef8781948ed6
1heart <yixin1996@gmail.com>
2016-06-14 14:33:55 -0400

    Linear assignment demo for MPEG7 shapes

--- code/linassgn/linassgn_demo.m
+++ code/linassgn/linassgn_demo.m
@@ -12,16 +12,43 @@
 % -------------------------------------------------------------------------
 
 DISP = 1;
-
-% Circle square demo
-m = 36; n = 36;
-circle = imread('images/circle.jpg');
-circle = double(rgb2gray(imresize(circle, [m n]))) ./ 255.0;
-square = imread('images/square.jpg');
-square = double(rgb2gray(imresize(square, [m n]))) ./ 255.0;
-
-distMatrix = construct_dist_matrix([m n]);
 lambdas = [0 0.0001 0.001 0.01 0.1];
 
-linassgn_warp(circle, square, distMatrix, lambdas, DISP);
+% Circle square demo
+% m = 36; n = 36;
+% circle = imread('images/circle.jpg');
+% circle = double(rgb2gray(imresize(circle, [m n]))) ./ 255.0;
+% square = imread('images/square.jpg');
+% square = double(rgb2gray(imresize(square, [m n]))) ./ 255.0;
+
+% distMatrix = construct_dist_matrix([m n]);
+
+% linassgn_warp(circle, square, distMatrix, lambdas, DISP);
+
+% MPEG7 datasets
+
+prefix = '/Users/yixin/amalthea/data/';
+load([prefix 'Coefficients/MPEG7_raw/apple.mat']);
+load([prefix 'Coefficients/MPEG7_raw/bird.mat']);
+
+m = 42; n = 42;
+distMatrix = construct_dist_matrix([m n]);
+
+imgs = { ...
+  apple01, ...
+  apple02, ...
+  bird01, ...
+};
+
+for i = 1:length(imgs)
+  curr = imgs{i};
+  currMax = max(curr);
+  imgs{i} = reshape(curr, [m n]) / currMax;
+end
+
+for i = 1:length(imgs)
+  for j = i+1:length(imgs)
+    linassgn_warp(imgs{i}, imgs{j}, distMatrix, lambdas, DISP);
+  end
+end
 
