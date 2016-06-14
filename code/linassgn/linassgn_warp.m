38a2b0dabc06c7f2f3c7e2322972810310683174
1heart <yixin1996@gmail.com>
2016-06-14 15:21:27 -0400

    New results from linear assignment demo on MPEG

--- code/linassgn/linassgn_warp.m
+++ code/linassgn/linassgn_warp.m
@@ -1,4 +1,34 @@
-function [] = linassgn_warp(source, target, distMatrix, lambdas, DISP)
+%--------------------------------------------------------------------------
+% Function:    linassgn_warp
+% Description: 	Warps two images as closely as possible for a given set of lambda.
+% Inputs: 
+%
+%	x,y 			- Images of the same size.
+% 
+%	distMatrix		- A cell array of the Euclidean distance between pairs of points.
+%
+% 	lambdas 			- The weightings of the distance matrix on the final results, e.g. [0 0.0001 0.001 0.01 0.1]
+% 	DISP  - A boolean to return the results or not.
+%
+% Outputs
+%
+%	new_xs - A cell array of the warped images.
+%
+% Usage: Used in hierarchical retrieval on the unit hypersphere.
+%
+% Authors(s):
+%   Mark Moyou - markmmoyou@gmail.com
+%	Yixin Lin - yixin1996@gmail.com
+% 	Glizela Taino - glizelataino@gmail.com
+%
+% Date: Monday 6th June, 2016 (2:34pm)
+%
+% Affiliation: Florida Institute of Technology. Information
+%              Characterization and Exploitation Laboratory.
+%              http://research2.fit.edu/ice/
+% -------------------------------------------------------------------------
+
+function [new_xs] = linassgn_warp(source, target, distMatrix, lambdas, DISP)
 
 if ~isequal(size(source), size(target)) error('Two images not equal!'); end;
 if ~isvector(lambdas) error('Lambda not a vector!'); end;
@@ -8,6 +38,7 @@ if (DISP) figure; end;
 
 [m n] = size(source); k = length(lambdas);
 x = source(:); y = target(:);
+new_xs = {};
 
 for i = 1:k
   lambda = lambdas(i);
@@ -15,6 +46,7 @@ for i = 1:k
   [rowsol, cost] = lapjv(C); % Get best linear assignment from x to y
   x_new = x(rowsol); % Find reconstructed shape
   img = reshape(x_new, [m n]);
+  new_xs = [new_xs img];
 
   if DISP
     subplot(2, k, i);
