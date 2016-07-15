2754913ff1cb4d812d08f8b3966ce82ed79ae85d
1heart <yixin1996@gmail.com>
2016-07-14 22:58:37 -0400

    Fix zip files for tex

--- writing/gen_zip.sh
+++ writing/gen_zip.sh
@@ -1,1 +1,1 @@
-pdflatex tech_report_1.tex -o tech_report_1.pdf && cp tech_report_1.pdf T5_techreport.pdf && zip -r T5_techreport.zip tech_report_1.tex tex img
+pdflatex tech_report_1.tex -o tech_report_1.pdf && cp tech_report_1.pdf T5_techreport.pdf && zip -r T5_techreport.zip tech_report_1.tex tech_report_1.bib tex img
